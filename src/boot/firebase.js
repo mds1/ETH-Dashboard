c8311da00bec82017127341440edf22a4595d78b
mds <17163988+mds1@users.noreply.github.com>
2020-04-11 14:05:31 -0700

    Add missing environment variables

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -13,6 +13,8 @@ const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 // Initialize Cloud Firestore through Firebase, and export variables for use
